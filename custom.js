c760338b989bdca01072917d0a859f92061450ad
gabriel-lumertz <glumertz10@gmail.com>
2023-06-08 01:16:04 -0300

    adicionado o header

---
+++ custom.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          cor1: '#D9D9D9',
+          cor2: '#593232',
+          cor3: '#BF7463',
+          cor4: '#F2AA6B',
+          cor5: '#F2DDB6',
+        }
+      }
+    }
+  }
